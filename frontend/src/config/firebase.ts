7689124c83c4d2ccf81403a0e69e90558abfb4d8
Yashtachi <yashwanthmiryala1234@gmail.com>
2025-09-26 00:52:36 +0530

    keys secured

--- frontend/src/config/firebase.ts
+++ frontend/src/config/firebase.ts
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD48DUKgs8aHHUKPdoRmVEBeuGyrg4c-gE",
-  authDomain: "neogenstudy-19044.firebaseapp.com",
-  projectId: "neogenstudy-19044",
-  storageBucket: "neogenstudy-19044.firebasestorage.app",
-  messagingSenderId: "614211439129",
-  appId: "1:614211439129:web:d92ba747c98418d508f888",
-  measurementId: "G-HSBEW4LMGL"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
